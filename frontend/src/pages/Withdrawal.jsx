c357a348185fe88f6bb0f95bbd5b31977ee63640
Moises Condori <moisescondori906@gmail.com>
2026-05-31 09:08:46 -0400

    fix: syntax error in Withdrawal.jsx

--- frontend/src/pages/Withdrawal.jsx
+++ frontend/src/pages/Withdrawal.jsx
@@ -492,7 +492,6 @@ export default function Withdrawal() {
                         {isInternar ? 'NIVEL INSUFICIENTE' : 'ENVIAR SOLICITUD'}
                       </Button>
                    </Card>
-                </section>
               </form>
 
               {/* Info Image */}
